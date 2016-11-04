9fd3235e36111fc453364a462c58655f2ae97b63
jmn05 <joseph@localhost.(none)>
2016-11-04 16:54:50 +0000

    Animation Prototype

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from tkinter import * #Imports tkinter for the GUI
+from time import * #Times is used for animations
 
 simulating = False #Boolean flag for when the simulation is running to stop certain functions from running
 
-window = tkinter.Tk() #Creates a new tkinter window
+window = Tk() #Creates a new tkinter window
 
 #This function fully closes python
 def quit():
@@ -35,6 +36,33 @@ quitButton.pack(side=LEFT)
 #creates a canvas for the virtual robots to move on
 C = Canvas(window,height=500,width=800,bg="white")
 C.pack()
+
+rectangle = C.create_rectangle(0,0,100,100)
+
+xspeed = 1
+yspeed = 1
+
+def animation():
+        global window
+        global xspeed
+        global yspeed
+        C.move(rectangle,xspeed,yspeed)
+        c = C.coords(rectangle)
+        print(c[3])
+        if c[2] > 499 or c[0] < 0:
+            yspeed = -yspeed
+            C.move(rectangle,xspeed,yspeed)
+
+            
+        if c[1] < 0 or c[1] > 799:
+            xspeed = -xspeed;
+            C.move(rectangle,xspeed,yspeed)
+            
+        window.after(16,animation)
+
+window.after(16,animation)
+
+
 
 window.mainloop()
 
