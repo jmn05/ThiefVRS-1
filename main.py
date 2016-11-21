6a284e9bbfd7b4c9e51e32caa6b19ae4ba0e30f3
Alexandru Emanuel Naciu <naciua@uni.coventry.ac.uk>
2016-11-21 20:23:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import * #Imports tkinter for the GUI
 from time import * #Times is used for animations
 from random import *
 from math import *
+import math
 simulating = False #Boolean flag for when the simulation is running to stop certain functions from running
 target = [0,0,0]
 count = 0
@@ -30,7 +31,7 @@ treasuresList = []
 #import of the file containing the thief image        
 photo = PhotoImage(file = "Thief.gif")
 photo2 = PhotoImage(file = "coin.gif")
-
+photo3 = PhotoImage(file = "cop.gif")
 
 #defines the class treasure
 class Treasure():
@@ -44,7 +45,7 @@ class Thief():
     def __init__(self):
         self.location = [randrange(0,750),randrange(0,450)]
         self.image2 = C.create_image(self.location[0],self.location[1],image=photo, anchor = CENTER)
-        self.speed = 0.0625
+        self.speed = 1
         self.move = 1
         self.notready = False
     def move_up(self):
@@ -71,8 +72,55 @@ class Thief():
             self.location[0] = target[0]
         C.coords(self.image2,self.location[0],self.location[1])
         C.update()
-        
+
+
+class Cop():
+    def __init__(self):
+        self.location = [randrange(50,750),randrange(50,550)]
+        self.speed = 1
+        self.move = 0.9
+        self.notready = False
+        self.copObj = C.create_image(self.location[0],self.location[1],image=photo3, anchor = CENTER)
+    def move_up(self):
+        self.location[1] = self.location[1] - self.move*self.speed
+        C.coords(self.copObj,self.location[0],self.location[1])
+        C.update()
+    def move_down(self):
+        self.location[1] = self.location[1] + self.move*self.speed
+        C.coords(self.copObj,self.location[0],self.location[1])
+        C.update()
+    def move_right(self):
+        self.location[0] = self.location[0] + self.move*self.speed
+        C.coords(self.copObj,self.location[0],self.location[1])
+        C.update()
+    def move_left(self):
+        self.location[0] = self.location[0] - self.move*self.speed
+        C.coords(self.copObj,self.location[0],self.location[1])
+        C.update()
+    def move_random(self):
+        self.ranlocation = [randrange(25,775),randrange(25,575)]
+        self.notready=True
+        while(self.notready):
+            if(self.ranlocation[0] > self.location[0]):
+                self.move_right()
+            if(self.ranlocation[0] < self.location[0]):
+                self.move_left()
+            if(self.ranlocation[1] > self.location[1]):
+                 self.move_down()
+            if(self.ranlocation[1] < self.location[1]):
+                 self.move_up()
+            if(math.fabs(round(self.location[0]) - self.ranlocation[0])<=1 and math.fabs(round(self.location[1]) - self.ranlocation[1])<=1):
+                self.notready=False
+            print(self.location, "TO", self.ranlocation)
+    def movemove(self):
+        while(True):
+            if(self.notready==False):
+                self.move_random()
+  
+
+
 thief = Thief()
+cop = Cop()
 
 #this function gets the nearest treasure based on the thief location
 def getNearestTreasure():
@@ -107,7 +155,15 @@ def thiefMove():
             else:
                 thief.move_up()
         C.delete(treasuresList.pop(int(target[2])).image)
-        
+
+def copMove():
+    while thief.speed != 0:
+        cop.move_random()
+
+def Movement():
+    thiefMove()
+    copMove()
+
 #this function deletes all the treasures and sets new random treasures
 def resetTreasures():
     global treasuresList
@@ -124,6 +180,8 @@ def callReset():
     resetTreasures()
     global thief
     thief = Thief()
+    global cop
+    cop = Cop()
 
 for i in range(0,numberOfTreasures):
     treasuresList.append(Treasure())
@@ -131,7 +189,7 @@ for i in range(0,numberOfTreasures):
 
 #Gives the buttons their various properties
 resetCanvas = Button(controlPanel, text="Reset", command= callReset)
-startButton = Button(controlPanel, text = "Start Simulation", command = thiefMove)
+startButton = Button(controlPanel, text = "Start Simulation", command = Movement)
 stopButton = Button(controlPanel,text = "End Simulation")
 quitButton = Button(controlPanel, text = "Quit", command= quit)
 
